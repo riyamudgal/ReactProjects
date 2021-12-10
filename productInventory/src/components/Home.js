5eecba86eb4a5cca76d8163e44b9b58e723419ff
Riya Mudgal <riyamudgal21@gmail.com>
2021-12-10 17:06:49 +0530

    update1

--- productInventory/src/components/Home.js
+++ productInventory/src/components/Home.js
@@ -5,6 +5,12 @@ function Home() {
         <div>
             Hello welcome !!
             Let's contibute to digital Environment by making phyiscal records to digital records.
+            <nav class="navbar navbar-light bg-light">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
         </div>
     )
 }
